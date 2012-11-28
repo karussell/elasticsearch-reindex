4137a4d388eed6edcd55f77d92305f110aa60512
Peter <peathal@yahoo.de>
2012-11-28 11:30:31 +0100

    added dependencies to plugin zip

--- reinstall.sh
+++ reinstall.sh
@@ -1,5 +1,5 @@
 ES=/usr/share/elasticsearch
 sudo $ES/bin/plugin remove reindex
 mvn -DskipTests clean package
-sudo $ES/bin/plugin -url ./target -install reindex
+sudo $ES/bin/plugin -url ./target/ -install reindex
 sudo service elasticsearch restart
