1735450ea7efbfabdee37db1038966a37dd26601
Peter <peathal@yahoo.de>
2013-09-24 13:36:31 +0200

    no system.err

--- src/test/java/com/pannous/es/reindex/ReIndexActionJsonTest.java
+++ src/test/java/com/pannous/es/reindex/ReIndexActionJsonTest.java
@@ -5,7 +5,7 @@ public class ReIndexActionJsonTest extends ReIndexActionTester {
     @Override
     protected MySearchResponse scrollSearch(String searchIndex, String type, String filter, int hits,
             boolean withVersion, int keepMinutes) {
-        System.err.println("you need to shutdown all local instances to run this test! " + getClass().getName());
+        // System.err.println("you need to shutdown all local instances to run this test! " + getClass().getName());
         return new MySearchResponseJson("localhost", 9200, searchIndex, type, filter, "", hits,
                 withVersion, keepMinutes);
     }
