1060494b88fb2a8ddfd9253d0995f2546cacb6e4
Itamar Syn-Hershko <itamar@code972.com>
2015-03-02 17:00:28 +0200

    Fixing build / install script

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,7 @@
 ES=/usr/share/elasticsearch
 sudo $ES/bin/plugin remove reindex
 mvn -DskipTests clean package
-FILE=`ls ./target/elasticsearch-*zip`
-sudo $ES/bin/plugin -url file:$FILE -install reindex
+sudo mkdir $ES/plugins/reindex
+sudo cp -a ./target/classes/com $ES/plugins/reindex
+sudo cp -a ./target/classes/es-plugin.properties $ES/plugins/reindex
 sudo service elasticsearch restart
